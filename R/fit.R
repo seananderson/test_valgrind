4f79cf8b9f6b71c5ea3ebebad517a29be3c4d9ad
Jim Thorson <James.T.Thorson@gmail.com>
2025-03-16 11:10:38 -0700

    first commit

---
+++ R/fit.R
@@ -0,0 +1,36 @@
+#' @title fit linreg
+#'
+#' @description fits linreg.
+#'
+#' @importFrom TMB MakeADFun sdreport
+#' @importFrom stats optim
+#'
+#' @param x independent.
+#' @param y dependent.
+#'
+#' @examples
+#' x = 1:10
+#' y = rnorm(10) + x
+#' out = fit( x=x, y=y )
+#'
+#' @useDynLib linreg, .registration = TRUE
+#' @export
+fit <-
+function( x, y ){
+  data <- list(Y = y, x=x)
+  parameters <- list(a=0, b=0, logSigma=0)
+  obj <- MakeADFun(data, parameters, DLL="linreg")
+  obj$hessian <- TRUE
+  opt <- do.call("optim", obj)
+  #opt
+  #opt$hessian ## <-- FD hessian from optim
+  #obj$he()    ## <-- Analytical hessian
+  sdrep = sdreport(obj)
+
+  out = list(
+    obj = obj,
+    opt = opt,
+    sdrep = sdrep
+  )
+  return(out)
+}
